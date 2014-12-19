a471de2197cc0fa45766ad2e6a6a6856fc17e9fb
tkmagesh <tkmagesh77@gmail.com>
2014-12-19 14:49:31 +0530

    Click Tracker - Seed

--- Day-01/01-AdvancedJavaScript/src/calculator.js
+++ Day-01/01-AdvancedJavaScript/src/calculator.js
@@ -1,5 +1,9 @@
-function add(x,y){
-	x = isNaN(x) ? 0 : parseInt(x,10);
-	y = isNaN(y) ? 0 : parseInt(y,10);
-	return x + y;
+function add(){
+	function parseArg(n){
+		if (n instanceof Array) return add.apply(this, n);
+		if (typeof n === "function") return parseArg(n());
+		return isNaN(n) ? 0 : parseInt(n,10);
+	}
+	return arguments.length <= 1 ? parseArg(arguments[0]) 
+		: parseArg(arguments[0]) + add([].slice.call(arguments,1));
 }
