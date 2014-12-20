8a0df70bcbbb092f00278417e78cb39aea08f002
tkmagesh <tkmagesh77@gmail.com>
2014-12-20 16:13:09 +0530

    Web Sockets

---
+++ Day-02/04-WebSockets/Server/chatApp.js
@@ -0,0 +1,10 @@
+var nodeJsWebSocket = require("nodejs-websocket");
+var server = nodeJsWebSocket.createServer(function(connection){
+	connection.on("text", function(msg){
+		server.connections.forEach(function(con){
+			con.sendText(msg);
+		});
+	});
+});
+server.listen("9090");
+console.log("Socket server listening on port 9090");
